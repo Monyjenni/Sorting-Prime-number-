1d0664f572ad769aaf8d7947111566fd432e24c8
Monyjenni <chansovanmonyyoeun03@gmail.com>
2022-07-08 16:21:19 +0700

    Third

---
+++ Prime number.py
@@ -0,0 +1,11 @@
+lower=900
+upper=1000
+print("The prime numbers in range are:")
+for number in range(lower,upper+1):
+    if number > 1:
+        for i in range(2,number):
+            #% mean take remainder
+            if(number%i)==0:
+                break
+    else:
+        print(number)
---
+++ sorting.py
@@ -0,0 +1,3 @@
+listA=[2,3,1,8,7,10,45,23]
+listA.sort()
+print ("This the sorting list_A",listA)
